59bc3b3b7676d84ac5f4ea2bdb29b6253193cfca
devendra <1997devendrabhopale@gmail.com>
2023-11-29 11:43:22 +0530

    first commit

---
+++ SpringBootChatApp/src/test/java/com/mca/springbootchatapp/SpringBootChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mca.springbootchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
